dad6d0192f3110aa4bf04a624ee77fa4aff14c2d
Saniyaayu Sekarpramesi <niyasekar@gmail.com>
2017-01-31 00:41:27 +0700

    Program Arduino untuk Load Game//Arduino Sketch for Loading Game

---
+++ arduino_sketch.ino
@@ -0,0 +1,11 @@
+void setup()
+{
+  Serial.begin(9600);
+               
+}
+void loop()
+{
+   int value = 1;    // read the value of the Analog Input
+   Serial.println(value);                     // Print the value; can also be seen on Serial Monitor
+    delay(1000);                                   // Don't send too fast or the Android buffer will flood - this worked for me
+}
